5591befded88d00c3245633b5cdf74780cf0c5dd
dendhi93 <dendhi.fendi@gmail.com>
2022-02-23 16:40:31 +0700

    prepare create Login.dart

---
+++ lib/Menus/Login.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _initViewLogin(context),);
+  }
+
+  Widget _initViewLogin(BuildContext context){
+
+  }
+
+}
