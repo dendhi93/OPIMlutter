42af9c35d5c22295f6b55aa49d27d2db352d25b6
dendhi93 <dendhi.fendi@gmail.com>
2022-03-22 13:10:01 +0700

    1.add lib http

---
+++ lib/Menus/adapter/BottomNavAdapter.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavAdapter extends StatefulWidget {
+  @override
+  _BottomNavAdapterState createState() => _BottomNavAdapterState();
+}
+
+class _BottomNavAdapterState extends State<BottomNavAdapter>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
