9c6ebe7317e7658db2317291c8d2e3da89c4e686
dendhi93 <dendhi.fendi@gmail.com>
2022-03-31 16:51:20 +0700

    create ui home

--- lib/Menus/Home/Home.dart
+++ lib/Menus/Home/Home.dart
@@ -19,8 +19,14 @@ class _HomeState extends State<Home>{
     return Form(
       child: new Column(
           children: <Widget>[
-            new Padding(padding: EdgeInsets.only(left: 60.0, top: 40.0)),
-              new Text("home",style:TextStyle(fontSize: 18)),
+            new Padding(padding: EdgeInsets.only(top: 40.0)),
+            new Stack(
+                children: <Widget>[
+                  new Image.asset('assets/images/opim_launcher.png', width: 290, height: 220,),
+                  new Padding(padding: EdgeInsets.only(top: 90.0)),
+                  new Text("home",style:TextStyle(fontSize: 18)),
+                ],
+            ),
           ],
       ),
     );
