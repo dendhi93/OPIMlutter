b1e066a69dc008206fe06a0888fc5b8f7bbcf465
dendhi93 <dendhi.fendi@gmail.com>
2022-04-13 14:27:07 +0700

    1. design Account.dart

--- lib/Menus/Account/Account.dart
+++ lib/Menus/Account/Account.dart
@@ -15,7 +15,7 @@ class _AccountState extends State<Account>{
 
   Widget _initViewAccount(BuildContext context){
     return Stack(
-      children: <Widget>[dashAccount, content],
+      children: <Widget>[dashAccount],
     );
   }
 
@@ -36,7 +36,7 @@ class _AccountState extends State<Account>{
                     children: <Widget>[
                       Padding(padding: EdgeInsets.only(top: 60.0)),
                       Image.asset('assets/images/ic_user.png', width: 90, height: 80,),
-                      Padding(padding: EdgeInsets.only(top: 15.0)),
+                      Padding(padding: EdgeInsets.only(top: 20.0)),
                       Text('Selamat Bekerja', style:TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold)),
                       Padding(padding: EdgeInsets.only(top: 10.0)),
                       Text('Kode Pengguna : 20105695', style:TextStyle(fontSize: 15, color: Colors.white)),
@@ -65,12 +65,145 @@ class _AccountState extends State<Account>{
             ),
         ),flex: 4,),
         Expanded(child: Container(
+          alignment: Alignment.topLeft,
           color: Colors.white,
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          child: SingleChildScrollView(
+            child:  Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                popColumn,
+                divisionColumn,
+                nikColumn,
+                imeiColumn,
+                lastLoginColumn,
+                registerDateColumn,
+                Padding(padding: EdgeInsets.only(top: 20.0)),
+                GestureDetector(
+                  onTap: (){ print("coba");},
+                  child: Expanded(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        Image.asset('assets/images/ic_logout.png', width: 50, height: 30,),
+                        Text('Keluar ', style:TextStyle(fontSize: 14, color: Colors.black)),
+                      ],
+                    ),
+                  )
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              ],
+            ),
+          )
         ),flex: 4,),
       ],
   );
 
-  get content => Container(
+  get popColumn => Column(
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP A ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Divider(color: HexColor("#D1D1D1"), height: 1),
+    ],
+  );
 
+  get divisionColumn => Column(
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('Divisi ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP A ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Divider(color: Colors.black, height: 1),
+    ],
+  );
+
+  get nikColumn => Column(
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('NIK ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP A ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Divider(color: HexColor("#D1D1D1"), height: 1),
+    ],
+  );
+
+  get imeiColumn => Column(
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('IMEI ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP A ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Divider(color: Colors.black, height: 1),
+    ],
+  );
+
+  get lastLoginColumn => Column(
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('Terakhir Login ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP A ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Divider(color: HexColor("#D1D1D1"), height: 1),
+    ],
+  );
+
+  get registerDateColumn => Column(
+    mainAxisSize: MainAxisSize.max,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('Tanggal Registrasi ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 5.0)),
+      Container(
+        alignment: Alignment.topLeft,
+        child: Text('POP A ', style:TextStyle(fontSize: 14, color: Colors.black)),
+      ),
+      Padding(padding: EdgeInsets.only(top: 10.0)),
+      Divider(color: Colors.black, height: 1),
+    ],
   );
 }
