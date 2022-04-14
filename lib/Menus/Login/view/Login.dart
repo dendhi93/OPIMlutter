c4724156470a91401b13e96c3ac6cd99940842ba
dendhi93 <dendhi.fendi@gmail.com>
2022-04-14 11:57:14 +0700

    1. set margin text field

--- lib/Menus/Login/view/Login.dart
+++ lib/Menus/Login/view/Login.dart
@@ -57,62 +57,68 @@ class _LoginState extends State<Login> implements LoginInterfaceView{
                         new Text("Oil Palm Integrated Management",
                             style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)
                         ),
-                        new Padding(padding: EdgeInsets.only(top: 45.0)),
-                        new TextFormField(
-                          controller: etLoginNik,
-                          decoration: new InputDecoration(
-                            prefixIcon: Icon(Icons.account_box),
-                            contentPadding: new EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-                            labelText: "NIK",
-                            fillColor: Colors.white,
-                            border: new OutlineInputBorder(
-                              borderRadius: new BorderRadius.circular(10.0),
-                              borderSide: new BorderSide(
+                        Container(
+                          margin: EdgeInsets.only(top: 45.0, left: 25.0,right: 25.0),
+                          child: TextFormField(
+                            controller: etLoginNik,
+                            decoration: new InputDecoration(
+                              prefixIcon: Icon(Icons.account_box),
+                              contentPadding: new EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                              labelText: "NIK",
+                              fillColor: Colors.white,
+                              border: new OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(10.0),
+                                borderSide: new BorderSide(
+                                ),
                               ),
                             ),
-                          ),
-                          validator: (val) {
-                            if(val.length==0) {return "Nik cannot be empty";
-                            }else{return null;}
-                          },
-                          keyboardType: TextInputType.text,
-                          maxLength: 15,
-                          style: new TextStyle(
-                            fontFamily: "Poppins",
+                            validator: (val) {
+                              if(val.length==0) {return "Nik cannot be empty";
+                              }else{return null;}
+                            },
+                            keyboardType: TextInputType.text,
+                            maxLength: 15,
+                            style: new TextStyle(
+                              fontFamily: "Poppins",
+                            ),
                           ),
                         ),
-                        new Padding(padding: EdgeInsets.only(top: 10.0)),
-                        new TextFormField(
-                          controller: etLoginPass,
-                          decoration: new InputDecoration(
-                            prefixIcon: Icon(Icons.lock),
-                            contentPadding: new EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-                            labelText: "Password",
-                            fillColor: Colors.white,
-                            border: new OutlineInputBorder(
-                              borderRadius: new BorderRadius.circular(10.0),
-                              borderSide: new BorderSide(
+
+                        Container(
+                          margin: EdgeInsets.only(top: 10.0, left: 25.0,right: 25.0, bottom: 50.0),
+                          child: TextFormField(
+                            controller: etLoginPass,
+                            decoration: new InputDecoration(
+                              prefixIcon: Icon(Icons.lock),
+                              contentPadding: new EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                              labelText: "Password",
+                              fillColor: Colors.white,
+                              border: new OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(10.0),
+                                borderSide: new BorderSide(
+                                ),
+                              ),
+                              suffixIcon: IconButton(onPressed: () { _toggle();},
+                                icon: Icon(Icons.remove_red_eye),
                               ),
                             ),
-                            suffixIcon: IconButton(onPressed: () { _toggle();},
-                              icon: Icon(Icons.remove_red_eye),
+                            validator: (val) {
+                              if(val.length==0) {return "password cannot be empty";
+                              }else{return null;}
+                            },
+                            obscureText: _obscureText,
+                            maxLength: 15,
+                            style: new TextStyle(
+                              fontFamily: "Poppins",
                             ),
                           ),
-                          validator: (val) {
-                            if(val.length==0) {return "password cannot be empty";
-                            }else{return null;}
-                          },
-                          obscureText: _obscureText,
-                          maxLength: 15,
-                          style: new TextStyle(
-                            fontFamily: "Poppins",
-                          ),
                         ),
-                        new Padding(padding: EdgeInsets.only(top: 50.0)),
+
+                        // new Padding(padding: EdgeInsets.only(top: 50.0)),
                         new RawMaterialButton(
                           fillColor: Colors.green,
                           splashColor: Colors.green,
-                          padding: EdgeInsets.only(left: 120, top:20, right: 120, bottom: 20),
+                          padding: EdgeInsets.only(left: 140, top:20, right: 140, bottom: 20),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(18.0),
                               side: BorderSide(color: Colors.green)
